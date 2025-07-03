6d2716f504a0aa31f5baa0fa060e762b8b8a7040
Mohammadrezamos <mohammadrezamoshafian6602@gmail.com>
2025-07-03 21:33:36 +0330

    FlashMessage Enum

---
+++ app/enums/flash_message.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class FlashMessage(Enum):
+    LOGIN_FAILED = ("Email or password is incorrect", "danger")
+
+    def message(self):
+        return self.value[0]
+
+    def category(self):
+        return self.value[1]
